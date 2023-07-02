8542b16966476151e53d4f49489471ab523eff63
Zach <zkdoran@gmail.com>
2023-07-02 16:28:26 -0400

    setting up photo storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,9 @@ Rails.application.configure do
   # Using the local settings for Active Storage
   config.active_storage.service = :local
 
+  # Using the Amazon S3 settings for Active Storage
+  config.active_storage.service = :amazon
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
